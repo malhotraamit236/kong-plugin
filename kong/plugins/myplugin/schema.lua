05d041bd5588262531ef6fa05601afe0fd1d0187
Amit Malhotra <amitmalhotra@ritual.co>
2020-05-03 14:09:20 -0400

    custom validator for rule conditions

--- kong/plugins/myplugin/schema.lua
+++ kong/plugins/myplugin/schema.lua
@@ -1,7 +1,34 @@
 local typedefs = require "kong.db.schema.typedefs"
+local lower = string.lower
 
 -- Grab pluginname from module name
 local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+
+local function table_length(tab)
+  local count = 0
+  for _ in pairs(tab) do count = count + 1 end
+  return count
+end
+
+local function get_case_insensitive_set(tab)
+  local keyset = {}
+  for k,v in pairs(tab) do
+    keyset[lower(k)]=v
+  end
+  return keyset
+end
+
+local function validate_rules(r)
+  
+  local original_keys_length = table_length(r.condition)
+  local case_insensitive_keys_set = get_case_insensitive_set(r.condition)
+  local case_insensitive_keys_length = table_length(case_insensitive_keys_set)
+  if original_keys_length ~= case_insensitive_keys_length then
+    return nil, "duplicate headers detected in one of the conditions"
+  end
+
+  return true
+end
 
 local condition_record = {
   type = "map",
@@ -20,7 +47,8 @@ local rules_array = {
     fields = {
       { condition = condition_record },
       { upstream_name = typedefs.name }
-    }
+    },
+    custom_validator = validate_rules
   }
 }
 
@@ -31,7 +59,7 @@ local schema = {
     { config = {
         type = "record",
         fields = {
-          { rules = rules_array}
+          { rules = rules_array }
         }
       },
     },
