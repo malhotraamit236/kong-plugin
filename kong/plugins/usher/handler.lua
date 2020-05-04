45baf1828128d8d5f4a832b2d6295b73cd48030b
Amit Malhotra <amitmalhotra@ritual.co>
2020-05-03 22:43:45 -0400

    change plugin object name

--- kong/plugins/usher/handler.lua
+++ kong/plugins/usher/handler.lua
@@ -1,12 +1,12 @@
 local access = require "kong.plugins.usher.access"
 
-local plugin = {
+local UsherPlugin = {
   PRIORITY = 900, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1",
 }
 
-function plugin:access(conf)
+function UsherPlugin:access(conf)
   access.execute(conf)
 end
 
-return plugin
+return UsherPlugin
